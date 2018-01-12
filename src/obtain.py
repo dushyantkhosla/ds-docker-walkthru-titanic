9e539de2946a31aad680d40fc3e6333d99b00397
Dushyant Khosla <dushyant.khosla@yahoo.com>
2018-01-12 16:41:46 +0000

    Added obtain.py to /src

---
+++ src/obtain.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+
+# Declare constants
+ROOT = '/home/ds-template-01/'
+PATH_TO_DATA_RAW = ROOT + 'data/00-raw/'
+
+URL_TITANIC = 'https://raw.githubusercontent.com/dushyantkhosla/tiny-datasets/master/titanic.csv'
+
+# Declare functions
+def get_titanic_data(filename='titanic_data.csv', url=URL_TITANIC, force_download=False):
+    """Download and cache the Titanic Data
+    
+    Parameters
+    ----------
+    filename: string (optional)
+        location to save the data
+    url: string (optional)
+        web location of the data
+    force_download: bool (optional)
+        if True, download the data again
+        
+    Returns
+    -------
+    df_raw: pandas.DataFrame
+        The Titanic data
+    """
+    import pandas as pd
+    if force_download or not os.path.exists(PATH_TO_DATA_RAW + filename):
+        df_raw = pd.read_csv(URL_TITANIC)
+    else:
+        df_raw = pd.read_csv(PATH_TO_DATA_RAW + filename)
+        
+    return df_raw
+        
+        
+        
